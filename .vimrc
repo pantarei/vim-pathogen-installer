35ea695b6be38ff2630c7ec55f71489c9626ad26
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-03 15:39:17 +0800

    Migrate from https://github.com/phpshift/vim-pathogen.

---
+++ .vimrc
@@ -0,0 +1,19 @@
+execute pathogen#infect()
+
+syntax on
+filetype on
+filetype plugin on
+filetype indent on
+
+set autoindent
+set background=dark
+set backupdir=/tmp
+set dir=/tmp
+set expandtab
+set shiftwidth=4
+set smartindent
+set swapfile
+set tabstop=4
+
+let g:php_cs_fixer_enable_default_mapping = 1
+let g:vim_markdown_folding_disabled=1
---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+set -o xtrace
+
+# Define variables.
+BRANCH="master"
+TMP_DIR=`mktemp -d`
+VIM_DIR=$HOME/.vim
+VIM_BUNDLE_DIR=$VIM_DIR/bundle
+
+# Backup current VIM configuration.
+mkdir -p $TMP_DIR/orig
+mv $HOME/.vim* $TMP_DIR/orig/
+
+# Clone repo into temp folder.
+cd $TMP_DIR
+git clone --recursive --branch $BRANCH https://github.com/phpshift/vim-pathogen-installer.git
+cp $TMP_DIR/vim-pathogen/.vimrc $HOME/
+
+# Fetch vim-pathogen.
+git clone https://github.com/tpope/vim-pathogen.git $VIM_DIR
+
+# Fetch latest bundles.
+git clone http://git.drupal.org/project/vimrc.git $VIM_BUNDLE_DIR/vim-drupal
+git clone https://github.com/StanAngeloff/php.vim $VIM_BUNDLE_DIR/vim-php
+git clone https://github.com/elzr/vim-json.git $VIM_BUNDLE_DIR/vim-json
+git clone https://github.com/evidens/vim-twig.git $VIM_BUNDLE_DIR/vim-twig
+git clone https://github.com/groenewege/vim-less $VIM_BUNDLE_DIR/vim-less
+git clone https://github.com/hail2u/vim-css3-syntax.git $VIM_BUNDLE_DIR/vim-css3-syntax
+git clone https://github.com/othree/html5.vim.git $VIM_BUNDLE_DIR/vim-html5
+git clone https://github.com/plasticboy/vim-markdown.git $VIM_BUNDLE_DIR/vim-markdown
+git clone https://github.com/saltstack/salt-vim.git $VIM_BUNDLE_DIR/vim-salt
+git clone https://github.com/scrooloose/syntastic.git $VIM_BUNDLE_DIR/vim-syntastic
+git clone https://github.com/sickill/vim-pasta.git $VIM_BUNDLE_DIR/vim-pasta
+git clone https://github.com/stephpy/vim-php-cs-fixer.git $VIM_BUNDLE_DIR/vim-php-cs-fixer
+git clone https://github.com/tokutake/twig-indent $VIM_BUNDLE_DIR/vim-twig-indent
+git clone https://github.com/tpope/vim-git $VIM_BUNDLE_DIR/vim-git
+git clone https://github.com/tpope/vim-sensible.git $VIM_BUNDLE_DIR/vim-sensible
