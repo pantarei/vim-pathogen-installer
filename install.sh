563a8831ac36a9a8c126597533ad348e6588c3de
Wong Hoi Sing Edison <hswong3i@gmail.com>
2014-07-11 09:58:01 +0800

    Handle GNU/OSX mktemp syntax different.

--- install.sh
+++ install.sh
@@ -4,7 +4,12 @@ set -o xtrace
 
 # Define variables.
 BRANCH="master"
+
 TMP_DIR=`mktemp -d`
+if [ -z "$TMP_DIR" ]; then
+    TMP_DIR=`mktemp -d -t ''`
+fi
+
 VIM_DIR=$HOME/.vim
 VIM_BUNDLE_DIR=$VIM_DIR/bundle
 
